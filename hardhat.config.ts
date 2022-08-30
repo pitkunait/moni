51d5ee6411cf6ec6e46c4d62b1abf0054f9be579
Vassili Kiritsenko <vassili.kiritsenko@gmail.com>
2022-08-30 21:45:44 +0300

    remove ds store

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -15,11 +15,11 @@ const config: HardhatUserConfig = {
     networks: {
         hardhat: {},
         groeli: {
-            url: "https://eth-goerli.g.alchemy.com/v2/oJ7c6qJ58omumdnU7XLT0r_-L6BkU_W5",
+            url: process.env.RINKEBY_API_KEY!,
             accounts: [process.env.RINKEBY_PRIVATE_KEY!]
         },
         rinkeby: {
-            url: "https://eth-rinkeby.alchemyapi.io/v2/7MyElH15sfKXQPf4HhENRvj0byrKa5N6",
+            url: process.env.GROELI_API_KEY!,
             accounts: [process.env.GROELI_PRIVATE_KEY!]
         }
     },
