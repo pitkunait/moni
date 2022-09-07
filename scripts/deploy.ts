28020d8d29083c46d590cac7ea57a978636678f3
Vassili Kiritsenko <vassili.kiritsenko@gmail.com>
2022-09-07 11:02:46 +0200

    update deploy script

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,13 +1,21 @@
 import { ethers } from "hardhat";
+import { readdirSync, readFileSync, writeFileSync } from "fs";
 
 async function main() {
-  const MoniNFT = await ethers.getContractFactory("MoniNFT");
-  const moniNFT = await MoniNFT.deploy("MoniNFT", "MONI");
-  await moniNFT.deployed();
-  console.log(`Contract deployed to ${moniNFT.address}`);
+    const MoniNFT = await ethers.getContractFactory("MoniNFT");
+    const moniNFT = await MoniNFT.deploy("MoniNFT", "MONI");
+    await moniNFT.deployed();
+    const path = 'artifacts/build-info';
+    const output = 'jsoninput/stdinput.json';
+    const files = readdirSync(path);
+    const rawdata = readFileSync(`${path}/${files[0]}`).toString();
+    const stdinput = JSON.parse(rawdata)['input'];
+    writeFileSync(output, JSON.stringify(stdinput));
+
+    console.log(`Contract deployed to ${moniNFT.address}`);
 }
 
 main().catch((error) => {
-  console.error(error);
-  process.exitCode = 1;
+    console.error(error);
+    process.exitCode = 1;
 });
